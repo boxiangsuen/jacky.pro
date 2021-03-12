da082944c702aa1372ecf0009374fa524d3ac9b7
sunboxiang <jacksunyx@163.com>
2021-03-13 07:02:55 +0800

    [2021-3-13 07:02:32] 1、swagger实现

---
+++ jacky.api/jacky.api/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace jacky.api.Controllers
+{
+    [Area("api/jacky")]
+    [ApiController]
+    public class HomeController : Controller
+    {
+        [HttpPost("show-index")]
+        public String Index()
+        {
+            return "Hello world!";
+        }
+    }
+}
